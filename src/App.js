642c99a15fecf10ae7ad2a7ad353a9ee0ef634e2
kimiakarbin <kimiakarbin@gmail.com>
2021-09-13 11:38:07 +0430

    commit on branch 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         hello kimia
+         hello kimia 2
         </p>
         <a
           className="App-link"
